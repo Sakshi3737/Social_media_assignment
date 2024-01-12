58473e0cc2acb261bc2aa74ee866c16b93df4e5e
Sakshi3737 <134384945+Sakshi3737@users.noreply.github.com>
2024-01-12 22:03:03 +0530

    create_post.js

---
+++ create_post.js
@@ -0,0 +1,14 @@
+    <script>
+      const container = document.querySelector(".container"),
+      privacy = container.querySelector(".post .privacy"),
+      arrowBack = container.querySelector(".audience .arrow-back");
+
+      privacy.addEventListener("click", () => {
+        container.classList.add("active");
+      });
+
+      arrowBack.addEventListener("click", () => {
+        container.classList.remove("active");
+      });
+    </script>
+    
